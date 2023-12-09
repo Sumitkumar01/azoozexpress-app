3e924f69bc32b5e72ae597350b8056713575c0f4
Sumit <sk7627577@gmail.com>
2023-12-09 19:30:19 +0530

    navbar complet

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Col, Row } from "react-bootstrap";
+import "./Navbar.css";
+
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Home", link: "/" },
@@ -25,94 +23,97 @@ const servicesNavigation = [
   },
 ];
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 function NavBar() {
   return (
-    <Navbar expand="lg" className="p-0">
+    <Navbar expand="lg" className="">
       <Container>
-        <div className="p-2">
-          <div className="pr-3">
-            <Navbar.Brand to="/" className="p-0 m-0">
-              <img
-                src="https://azoozexpress.com/wp-content/uploads/2023/07/Englishlogopngblackpng-1-1.png"
-                className="img-fluid"
-                alt=""
-              />
-            </Navbar.Brand>
-          </div>
-        </div>
-        <div>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              {navigation.map((item) => (
-                <Nav.Link
-                  key={item.name}
+        <Navbar.Brand to="#" className="pe-5 py-0">
+          <img
+            width="148"
+            height="65"
+            src="https://azoozexpress.com/wp-content/uploads/2023/07/Englishlogopngblackpng-1-1.png"
+            class="img-fluid"
+            alt=""
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="bg-warning"
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-3 ps-3">
+            {navigation.map((item) => (
+              <Nav.Link
+                as={Link}
+                to={item.link}
+                className="fs-5 text-light ff-m text-md-center  text-lg-start bg-sm-warning bg-md-warning"
+              >
+                {item.name}
+              </Nav.Link>
+            ))}
+            <NavDropdown
+              title="Service"
+              id="basic-nav-dropdown"
+              className="fs-5 text-light ff-m rounded-0 text-md-center  text-lg-start"
+            >
+              {servicesNavigation.map((item) => (
+                <NavDropdown.Item
                   as={Link}
                   to={item.link}
-                  className="text-white"
+                  className="text-danger bg-warning text-md-center  text-lg-start ff-p"
                 >
                   {item.name}
-                </Nav.Link>
+                </NavDropdown.Item>
               ))}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
 
-              <NavDropdown
-                title="Services"
-                id="basic-nav-dropdown"
-                className="text-white "
-              >
-                {servicesNavigation.map((item) => (
-                  <NavDropdown.Item
-                    key={item.name}
-                    as={Link}
-                    to={item.link}
-                    className="bg-warning text-white"
-                  >
-                    {item.name}
-                  </NavDropdown.Item>
-                ))}
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-        <div className="p-1  d-sm-none d-md-none d-lg-block d-xl-blok">
-          <Row className="p-2 ">
-            <Col>
-              <Link to="#">
-                <span className="visually-hidden">Facebook</span>
-                <FaFacebook className="social-icon-text" />
-              </Link>
-            </Col>
-            <Col>
-              <Link>
-                <span className="visually-hidden">Twitter</span>
-                <FaTwitter className="social-icon-text" />
-              </Link>
-            </Col>
-            <Col>
-              <Link>
-                <span className="visually-hidden">Instagram</span>
-                <FaInstagram className="social-icon-text" />
-              </Link>
-            </Col>
-            <Col>
-              <Link>
-                <span className="visually-hidden">Linkedin</span>
-                <FaLinkedin className="social-icon-text" />
-              </Link>
-            </Col>
-          </Row>
-        </div>
-        <Row className="p-1 ">
-          <div className="p-r-0">
-            <Link
-              className="btn btn-danger btn-lg py-4 px-5"
-              href="http://65.1.2.151/azooz-contact-page/"
-            >
-              Get Your Quote
-            </Link>
+        {/* social-link */}
+        <div className="row me-lg-3">
+          <div className="col-6 p-1 d-none  d-sm-none d-md-none d-lg-block d-xl-blok">
+            <div className="p-2 div row">
+              <div className="col-3">
+                <Link to="#">
+                  <span className="visually-hidden">Facebook</span>
+                  <FaFacebook className="social-icon-text" />
+                </Link>
+              </div>
+              <div className="col-3">
+                <Link>
+                  <span className="visually-hidden">Twitter</span>
+                  <FaTwitter className="social-icon-text" />
+                </Link>
+              </div>
+              <div className="col-3">
+                <Link>
+                  <span className="visually-hidden">Instagram</span>
+                  <FaInstagram className="social-icon-text" />
+                </Link>
+              </div>
+              <div className="col-3">
+                <Link>
+                  <span className="visually-hidden">Linkedin</span>
+                  <FaLinkedin className="social-icon-text" />
+                </Link>
+              </div>
+            </div>
           </div>
-        </Row>
+          <div className="col-6 p-1 d-none d-sm-none d-md-none d-lg-block d-xl-blok">
+            <div className="text-center">
+              <Link
+                className="btn btn-danger"
+                to="http:65.1.2.151/azooz-contact-page/"
+              >
+                Get Your Quote
+              </Link>
+            </div>
+          </div>
+        </div>
       </Container>
     </Navbar>
   );
