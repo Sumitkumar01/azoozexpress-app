7f48201b891705ec147a23b15e6ccfd97de5a634
Sumit <sk7627577@gmail.com>
2023-12-09 20:35:08 +0530

    completed

--- src/features/home/components/Solution.js
+++ src/features/home/components/Solution.js
@@ -4,196 +4,205 @@ import React from "react";
 
 function Solution() {
   return (
-    <div className="container my-3 p-3">
+    <div className=" my-3 p-3">
       {/* heading */}
       <section>
         <div className="container text-center">
-          <h2 className="fw-bolder fs-1 ff-m">
+          <h2 className="fw-bolder fs-5 ff-m">
             ONE <span className="text-danger">STOP SOLUTION</span>
           </h2>
         </div>
       </section>
       {/* items */}
-      <section>
-        <div className="row aling-item-center justify-content-center">
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center bg-warning br">
-              <div className="col-4 aling-item-center justify-content-center cp-1">
+      <div className="container">
+        <section>
+          <div className="row">
+            <section className="col-lg-4 p-2">
+              <div className="row bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center justify-content-center ">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="47"
+                    height="57"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-4.png"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
 
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="47"
-                  height="57"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-4.png"
-                  className="img-fluid 1"
-                  alt=""
-                />
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center justify-content-center">
+                  <h2 className="fs-5 ff-p fw-medium">
+                    Documents and Parcel Delivery
+                  </h2>
+                </div>
               </div>
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center justify-content-center ">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="57"
+                    height="57"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/confidential-document-documents-svgrepo-com-1-1.png"
+                    className="img-fluid 2"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center justify-content-center">
+                  <h2 className="fs-5 ff-p fw-medium">
+                    Confidential Documents Deliver
+                  </h2>
+                </div>
+              </div>
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center  bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center justify-content-center ">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="57"
+                    height="60"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-162-1.png"
+                    className="img-fluid 3"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center justify-content-center">
+                  <h2 className="fs-5 ff-p fw-medium">
+                    Corporate Mail Bags Services
+                  </h2>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section>
+          <div className="row">
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="62"
+                    height="64"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-164-1.png"
+                    className="img-fluid 4"
+                    alt=""
+                  />
+                </div>
 
-              <div className="col-8 aling-item-center justify-content-center">
-                <h2 className="cfs-18">Documents and Parcel Delivery</h2>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">
+                    Corporate Mail Room Management
+                  </h2>
+                </div>
               </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center bg-warning br">
-              <div className="col-4 aling-item-center justify-content-center cp-1">
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="60"
+                    height="60"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/cash-pay-svgrepo-com-1-1.png"
+                    className="img-fluid 5"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">Cash on Delivery(COD)</h2>
+                </div>
+              </div>
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="60"
+                    height="60"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-171-1.png"
+                    className="img-fluid 6"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">
+                    Same-Day Courier Service
+                  </h2>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section>
+          <div className="row ">
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center  bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="45"
+                    height="50"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Vector-2-1.png"
+                    className="img-fluid 7"
+                    alt=""
+                  />
+                </div>
 
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="57"
-                  height="57"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/confidential-document-documents-svgrepo-com-1-1.png"
-                  className="img-fluid 2"
-                  alt=""
-                />
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">Courier on Call</h2>
+                </div>
               </div>
-              <div className="col-8 aling-item-center justify-content-center">
-                <h2 className="cfs-18">Confidential Documents Deliver</h2>
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center  bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="60"
+                    height="60"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-176-1.png"
+                    className="img-fluid 8"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">Drop and Ship</h2>
+                </div>
               </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center  bg-warning br">
-              <div className="col-4 aling-item-center justify-content-center cp-1">
-
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="57"
-                  height="60"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-162-1.png"
-                  className="img-fluid 3"
-                  alt=""
-                />
+            </section>
+            <section className="col-lg-4  p-2">
+              <div className="row aling-item-center  bg-warning br  m-0">
+                <div className="col-4 d-flex m-0  p-1   aling-item-center justify-content-center">
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width="79"
+                    height="64"
+                    src="https://azoozexpress.com/wp-content/uploads/2023/07/Vector-3-1.png"
+                    className="img-fluid 9"
+                    alt=""
+                  />
+                </div>
+                <div className="col-8 d-flex m-0  p-2  m-0 aling-item-center">
+                  <h2 className="fs-5 ff-p fw-medium">Fulfilment service</h2>
+                </div>
               </div>
-              <div className="col-8 aling-item-center justify-content-center">
-                <h2 className="cfs-18">Corporate Mail Bags Services</h2>
-              </div>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section>
-        <div className="row aling-item-center justify-content-center">
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="62"
-                  height="64"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-164-1.png"
-                  className="img-fluid 4"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Corporate Mail Room Management</h2>
-              </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="60"
-                  height="60"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/cash-pay-svgrepo-com-1-1.png"
-                  className="img-fluid 5"
-                  alt=""
-                />
-              </div>
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Cash on Delivery(COD)</h2>
-              </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="60"
-                  height="60"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-171-1.png"
-                  className="img-fluid 6"
-                  alt=""
-                />
-              </div>
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Same-Day Courier Service</h2>
-              </div>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section>
-        <div className="row aling-item-center justify-content-center">
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center  bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="45"
-                  height="50"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Vector-2-1.png"
-                  className="img-fluid 7"
-                  alt=""
-                />
-              </div>
-
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Courier on Call</h2>
-              </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center  bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="60"
-                  height="60"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Group-176-1.png"
-                  className="img-fluid 8"
-                  alt=""
-                />
-              </div>
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Drop and Ship</h2>
-              </div>
-            </div>
-          </section>
-          <section className="col-lg-4 aling-item-center justify-content-center p-2">
-            <div className="row aling-item-center  bg-warning br">
-              <div className="col-4 aling-item-center">
-                <img
-                  loading="lazy"
-                  decoding="async"
-                  width="79"
-                  height="64"
-                  src="https://azoozexpress.com/wp-content/uploads/2023/07/Vector-3-1.png"
-                  className="img-fluid 9"
-                  alt=""
-                />
-              </div>
-              <div className="col-8 aling-item-center">
-                <h2 className="cfs-18">Fulfilment service</h2>
-              </div>
-            </div>
-          </section>
-        </div>
-      </section>
+            </section>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
