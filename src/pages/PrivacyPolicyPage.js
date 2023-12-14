670160bbc2b413be6fdc8e2534bfa2f6155f14de
Sumit <sk7627577@gmail.com>
2023-12-14 14:05:51 +0530

    complite

--- src/pages/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage.js
@@ -1,9 +1,172 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../features/navbar/Navbar";
+import Footer from "../features/footer/Footer";
 
 function PrivacyPolicyPage() {
+  const entrytitle = "fs-1 fw-bold";
+  const pageHeader = "p-3";
+  const BlockHeading = "fs-2 bold";
+  const wpBlockHeading = "fs-3 bold";
+  const pCName = "ff-p fs-6";
   return (
-    <div>PrivacyPolicyPage</div>
-  )
+    <>
+      <div className="container mt-3">
+        <header className={pageHeader}>
+          <h1 className={entrytitle}>Privacy Policy</h1>
+        </header>
+        <div>
+          <p className={pCName}>
+            <strong>Effective Date:</strong> 15.06.2023
+          </p>
+          <p className={pCName}>
+            At Azooz Express, we value the privacy and security of our users.
+            This Privacy Policy outlines how we collect, use, store, and protect
+            your personal information when you use our website or services.
+            Please read this policy carefully to understand our practices
+            regarding your personal data.
+          </p>
+          <h2 className={BlockHeading}>Information We Collect</h2>
+          <h3 className={wpBlockHeading}>1.1 Personal Information</h3>
+          <p className={pCName}>
+            We may collect personal information from you when you visit our
+            website, create an account, place an order, or communicate with our
+            customer support. The types of personal information we may collect
+            include:
+          </p>
+          <h3 className={wpBlockHeading}>1.1 Personal Information</h3>
+          <p className={pCName}>
+            <strong>Full Name</strong>
+            <br />
+            Contact information (phone number, email address, delivery address)
+            <br />
+            Payment details (credit card information, billing address)
+            <br />
+            Account credentials (username, password)
+            <br />
+            1.2 Automatically Collected Information
+            <br />
+            When you interact with our website, we may automatically collect
+            certain information about your device and usage. This may include:
+          </p>
+          <h2 className={BlockHeading}>IP Address</h2>
+          <p className={pCName}>
+            Browser type and version
+            <br />
+            Operating system
+            <br />
+            Pages visited and actions taken
+            <br />
+            Date and time of visits
+            <br />
+            Referring website addresses
+          </p>
+          <h2 className={BlockHeading}>Use of Information</h2>
+          <h3 className={wpBlockHeading}>2.1 Personal Information</h3>
+          <p className={pCName}>
+            We use the personal information we collect for the following
+            purposes:
+          </p>
+          <p className={pCName}>
+            <strong>Processing and delivering your orders</strong>
+            <br />
+            Communicating with you regarding your orders or inquiries
+            <br />
+            Providing customer support
+            <br />
+            Sending you updates, promotions, and newsletters (with your consent)
+            <br />
+            Improving our website, services, and overall customer experience
+            <br />
+            Complying with legal obligations
+            <br />
+          </p>
+          <h3 className={wpBlockHeading}>
+            2.2 Automatically Collected Information
+          </h3>
+          <p className={pCName}>
+            We use automatically-collected information to analyze website usage,
+            improve our services, and enhance the security and functionality of
+            our website. This information is generally aggregated and does not
+            personally identify you.
+          </p>
+          <h2 className={BlockHeading}>Data Sharing &amp; Disclosure</h2>
+          <h3 className={wpBlockHeading}>3.1 Third-Party Service Providers</h3>
+          <p className={pCName}>
+            We may engage trusted third-party service providers to perform
+            certain functions on our behalf, such as payment processing,
+            delivery services, or analytics. These service providers will have
+            access to your personal information only to the extent necessary to
+            perform their functions and are contractually obligated to maintain
+            its confidentiality.
+          </p>
+          <h3 className={wpBlockHeading}>3.2 Legal Requirements</h3>
+          <p className={pCName}>
+            We may disclose your personal information if required to do so by
+            law or in response to a valid legal request, such as a court order,
+            government inquiry, or to protect our rights, property, or safety.
+          </p>
+          <h2 className={BlockHeading}>Data Security</h2>
+          <p className={pCName}>
+            We take appropriate technical and organizational measures to
+            safeguard your personal information and protect it against
+            unauthorized access, alteration, disclosure, or destruction.
+            However, please note that no data transmission over the internet or
+            storage method is 100% secure. While we strive to use commercially
+            acceptable means to protect your personal information, we cannot
+            guarantee its absolute security.
+          </p>
+          <h2 className={BlockHeading}>Your Rights</h2>
+          <p className={pCName}>
+            You have certain rights regarding your personal information,
+            including the right to:
+          </p>
+          <p className={pCName}>
+            Access and obtain a copy of your personal information
+            <br />
+            Correct or update your personal information
+            <br />
+            Request deletion of your personal information
+            <br />
+            Object to the processing of your personal information
+            <br />
+            Withdraw your consent (where applicable)
+            <br />
+            To exercise any of these rights, please contact us using the contact
+            information provided below.
+          </p>
+          <h2 className={BlockHeading}>Third-Party Links</h2>
+          <p className={pCName}>
+            Our website may contain links to third-party websites or services.
+            We are not responsible for the privacy practices or content of these
+            third-party sites. We encourage you to read the privacy policies of
+            any third-party sites you visit.
+          </p>
+          <h2 className={BlockHeading}>Changes to the Privacy Policy</h2>
+          <p className={pCName}>
+            We may update this Privacy Policy from time to time. Any changes we
+            make will be posted on this page, and the effective date will be
+            revised accordingly. We encourage you to review this policy
+            periodically for any updates.
+          </p>
+          <h2 className={BlockHeading}>Contact Us</h2>
+          <p className={pCName}>
+            If you have any questions, concerns, or requests regarding this
+            Privacy Policy or our data practices, please contact us at:
+          </p>
+          <h2 className={BlockHeading}>Azooz Express</h2>
+          <p className={pCName}>
+            PO BOX 84, Postal code 101, Sultanate of Oman.
+            <br />
+            Muscat, Oman
+            <br />
+            Email: info@azoozexpress.com
+          </p>
+          <div className="post-tags" />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default PrivacyPolicyPage
+export default PrivacyPolicyPage;
--- src/pages/TermsOfServicePage.js
+++ src/pages/TermsOfServicePage.js
@@ -1,9 +1,155 @@
 import React from 'react'
+import Footer from '../features/footer/Footer';
 
 function TermsOfServicePage() {
+
+  const entrytitle ="fs-1 fw-bold"
+  const pageHeader ="p-3"
+  const BlockHeading="fs-2 bold"
+  const wpBlockHeading="fs-3 bold"
+  const pCName="ff-p fs-6"
   return (
-    <div>TermsOfServicePage</div>
-  )
+    <div>
+      <div className="container p-5">
+        <header className={pageHeader}>
+          <h1 className={entrytitle}>Terms of Service</h1>
+        </header>
+        <div>
+          <p className={pCName}>Effective Date: 15.06.2023</p>
+          <p className={pCName}>
+            Please read these Terms and Conditions (“Terms”) carefully before
+            using the Azooz Express website and services. These Terms govern
+            your access to and use of our website, as well as any orders or
+            transactions you make through our platform. By accessing or using
+            our website, you agree to be bound by these Terms. If you do not
+            agree to these Terms, please refrain from using our services.
+          </p>
+          <h2 className={BlockHeading}>Use of Services</h2>
+          <h3 className={wpBlockHeading}>1.1 Eligibility</h3>
+          <p className={pCName}>
+            You must be at least 18 years old to use our services and create an
+            account. By using our services, you represent and warrant that you
+            have the legal capacity to enter into a binding agreement.
+          </p>
+          <h3 className={wpBlockHeading}>1.2 Account Creation</h3>
+          <p className={pCName}>
+            To use certain features of our services, you may be required to
+            create an account. You are responsible for maintaining the
+            confidentiality of your account credentials and for all activities
+            that occur under your account. You agree to provide accurate and
+            complete information when creating an account.
+          </p>
+          <h3 className={wpBlockHeading}>1.3 Prohibited Activities</h3>
+          <p className={pCName}>When using our services, you agree not to:</p>
+          <p className={pCName}>
+            Violate any applicable laws, regulations, or third-party rights
+            <br />
+            Use our services for any illegal, unauthorized, or unethical
+            purposes
+            <br />
+            Interfere with the proper functioning of our website or services
+            <br />
+            Engage in fraudulent activities or misrepresent yourself or your
+            intentions
+            <br />
+            Collect or store personal information of other users without their
+            consent
+            <br />
+            Use automated scripts or bots to access or use our services
+          </p>
+          <h2 className={BlockHeading}>Orders &amp; Payments</h2>
+          <h3 className={wpBlockHeading}>2.1 Placing Orders</h3>
+          <p className={pCName}>
+            You may place orders through our website by providing accurate and
+            complete information, including delivery address and payment
+            details. By placing an order, you represent that you are authorized
+            to use the provided payment method.
+          </p>
+          <h3 className={wpBlockHeading}>
+            2.2 Order Confirmation &amp; Acceptance
+          </h3>
+          <p className={pCName}>
+            After placing an order, you will receive an order confirmation
+            email. Please note that this email is not a guarantee of acceptance.
+            We reserve the right to accept or decline your order at our sole
+            discretion.
+          </p>
+          <h3 className={wpBlockHeading}>2.3 Pricing &amp; Payment</h3>
+          <p className={pCName}>
+            All prices listed on our website are in the local currency and are
+            subject to change without notice. Payments are processed securely
+            through our authorized payment providers. By providing your payment
+            information, you authorize us to charge the applicable fees for your
+            orders.
+          </p>
+          <h3 className={wpBlockHeading}>2.4 Cancellation &amp; Refunds</h3>
+          <p className={pCName}>
+            Cancellation and refund policies may vary depending on the type of
+            order and applicable laws. Please refer to our cancellation and
+            refund policy or contact our customer support for more information.
+          </p>
+          <h2 className={BlockHeading}>Intellectual Property</h2>
+          <p className={pCName}>
+            All intellectual property rights in our website and services,
+            including but not limited to trademarks, logos, text, graphics, and
+            software, are owned by or licensed to Azooz Express. You are granted
+            a limited, non-exclusive, and non-transferable license to access and
+            use our services for personal, non-commercial purposes. You must not
+            reproduce, modify, distribute, or exploit any content from our
+            website without our prior written consent.
+          </p>
+          <h2 className={wpBlockHeading}>Limitation of Liability</h2>
+          <p className={pCName}>
+            To the fullest extent permitted by applicable law, Azooz Express and
+            its affiliates, directors, officers, employees, and agents shall not
+            be liable for any direct, indirect, incidental, consequential, or
+            punitive damages arising out of or in connection with your use of
+            our website or services, including but not limited to loss of
+            profits, data, or business opportunities.
+          </p>
+          <h2 className={BlockHeading}>Indemnification</h2>
+          <p className={pCName}>
+            You agree to indemnify and hold Azooz Express and its affiliates,
+            directors, officers, employees, and agents harmless from any claims,
+            demands, losses, liabilities, damages, costs, and expenses
+            (including attorneys’ fees) arising out of or in connection with
+            your use of our website or services, your violation of these Terms,
+            or your violation of any rights of a third party.
+          </p>
+          <h2 className={BlockHeading}>Governing Law &amp; Jurisdiction</h2>
+          <p className={pCName}>
+            These Terms shall be governed by and construed in accordance with
+            the laws of Oman. Any disputes arising out of or relating to these
+            Terms or your use of our services shall be subject to the exclusive
+            jurisdiction of the courts of Muscat, Oman.
+          </p>
+          <h2 className={BlockHeading}>Changes to the Terms</h2>
+          <p className={pCName}>
+            We reserve the right to modify or update these Terms at any time
+            without prior notice. Any changes will be effective immediately upon
+            posting on our website. Your continued use of our services after the
+            posting of revised Terms constitutes your acceptance of such
+            changes.
+          </p>
+          <h2 className={BlockHeading}>Contact Us</h2>
+          <p className={pCName}>
+            If you have any questions, concerns, or inquiries regarding these
+            Terms or our services, please contact us at:
+          </p>
+          <h2 className={BlockHeading}>Azooz Express</h2>
+          <p className={pCName}>
+            PO BOX 84, Postal code 101, Sultanate of Oman.
+            <br />
+            Muscat, Oman
+            <br />
+            Email: info@azoozexpress.com
+          </p>
+          <div className="post-tags" />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default TermsOfServicePage
