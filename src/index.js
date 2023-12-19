e9f247fabb6ccff76aa8de61e43c47941b070b9f
Sumit <sk7627577@gmail.com>
2023-12-19 10:32:25 +0530

    addin bootstrap link

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-
+import'../node_modules/react-bootstrap/dist/react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
